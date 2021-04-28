bb78e3f9e330a7fc03d9aaf1c76bdd55e3bffa26
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-04-29 00:11:21 +0530

    Add files via upload

---
+++ Admins.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class Admins
+    {
+        public string email;
+        public string password;
+    }
+}
---
+++ Employeedata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class Employeedata
+    {
+
+        public int EmployeeId;
+        public string EmployeeName;
+        public string EmployeeDesignation;
+        public int EmployeeSalary;
+    }
+}
