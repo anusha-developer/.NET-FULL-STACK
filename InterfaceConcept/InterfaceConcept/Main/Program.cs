9041c215a4481096428ac1795243ad4a31d0330a
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-07-05 14:48:28 +0530

    Add files via upload

---
+++ InterfaceConcept/InterfaceConcept/Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceConcept.Repositary;
+using System;
+
+namespace InterfaceConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentRepositaryImplementation studentRepositary = new StudentRepositaryImplementation();
+            Console.WriteLine(studentRepositary.addstudent());
+            Console.WriteLine(studentRepositary.deletestudent());
+            Console.WriteLine(studentRepositary.getAllStudents());
+            Console.WriteLine(studentRepositary.getStudentById());
+            Console.WriteLine(studentRepositary.updateStudent());
+           
+            /*studentRepositary.addstudent();
+            studentRepositary.deletestudent();
+            studentRepositary.getAllStudents();
+            studentRepositary.getStudentById();
+            studentRepositary.updateStudent();*/
+        }
+    }
+}
---
+++ InterfaceConcept/InterfaceConcept/Repositary/StudentRepositaryImplementation.cs
@@ -0,0 +1,35 @@
+﻿using InterfaceConcept.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceConcept.Repositary
+{
+    class StudentRepositaryImplementation : IStudentRepositary
+    {
+        public string addstudent()
+        {
+            return "Student Added Success";
+        }
+
+        public string deletestudent()
+        {
+            return " Student  details deleted ";
+        }
+
+        public string getAllStudents()
+        {
+            return " Retrived all the details";
+        }
+
+        public string getStudentById()
+        {
+            return "retrived all the details";
+        }
+
+        public string updateStudent()
+        {
+            return "Updated Student details";
+        }
+    }
+}
