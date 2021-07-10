b39e878a9020db061e52297ab3d1792c94622fec
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-07-10 23:13:49 +0530

    Add files via upload

---
+++ Encapsulation/Encapsulation/Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using Encapsulation.Model;
+using System;
+
+namespace Encapsulation
+{   
+    //Eccapsulation is mainly used for hiding the data
+    // it contain methods & variables by using single class
+    //it is used for security purpose
+    //we are making feilds as a private
+    // in private class we use getters & setters we can access the data
+    //in setters &getters mathods we used as public method to access the data
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //without mouse over we can throw an error to type the Alt&Enter
+            Actor actor = new Actor();
+            actor.actorId = 10;
+            actor.name = "Anusha";
+            actor.nooffilmsacted = 5;
+            actor.experience = 3;
+            actor.favoritefood = "Chicken";
+            Console.WriteLine(actor);
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Model/Actor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation.Model
+{
+    class Actor
+    {
+        private int ActorId;
+        private string Name;
+        private int NoOffilmsActed;
+        private int Experience;
+        private string FavroiteFood;
+
+        public int actorId
+        {
+             get { return ActorId; }
+            set { ActorId = value; }
+        }
+        public string name 
+        {
+            get { return Name;}
+            set { Name = value; }
+        
+        }
+        public int nooffilmsacted
+        {
+            get { return NoOffilmsActed; }
+            set { NoOffilmsActed = value; }
+        }
+        public  int experience
+        {
+            get { return Experience; }
+            set { Experience = value; }
+        }
+        public string favoritefood
+        {
+            get { return FavroiteFood; }
+            set { FavroiteFood = value; }
+        }
+
+        //by using this we can the the all the data without using number of  consoles
+        // we can access the data in single override class
+        public override string ToString()
+        {
+            return "\nActorId:" + actorId + "\nName:" + name + "\nNoOfFilmsActed:" + nooffilmsacted +
+                         "\nExperience:" + experience + "\nFavoriteFood:" + favoritefood ;
+        }
+    }
+    }
+
+ 
