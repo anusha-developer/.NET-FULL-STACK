bba139fe2ffcb15eff8a2c6453c1d23e6b789768
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-07-10 11:42:55 +0530

    Add files via upload

---
+++ EnumProgram/EnumProgram/Model/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumProgram.Model
+{
+   enum Roles  // Enum is a Keyword  Constant values
+    {
+        Admin,
+        Superadmin,
+        User,
+    }
+}
---
+++ EnumProgram/EnumProgram/main/Program.cs
@@ -0,0 +1,30 @@
+﻿using EnumProgram.Model;
+using System;
+
+namespace EnumProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Enum Creation
+            Colors colors = Colors.BLUE;
+            Console.WriteLine(colors);
+
+            Roles roles = Roles.Admin;
+            Console.WriteLine(roles);
+            //we can convert string into string
+            //we can convert the object to object
+            //we can can't obj to string
+            if (roles.ToString()=="Admin")
+                // roles is a object it can covert to string
+            {
+                Console.WriteLine("Welcome to  adminpage");
+            }
+            else
+            {
+                Console.WriteLine("You are navigated to Home");
+            }
+        }
+    }
+}
