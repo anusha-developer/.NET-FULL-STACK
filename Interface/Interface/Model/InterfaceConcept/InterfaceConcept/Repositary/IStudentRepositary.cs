3ecb40532e5a4038186cfdc37b8995db0a5b8547
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-29 16:39:43 +0530

    Add files via upload

---
+++ Interface/Interface/Model/InterfaceConcept/InterfaceConcept/Repositary/IStudentRepositary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceConcept.Interface
+{
+    public  interface IStudentRepositary
+    {
+        public string addstudent();
+
+        public string getAllStudents();
+
+        public string deletestudent();
+
+        public string getStudentById();
+
+        public string updateStudent();
+    }
+}
