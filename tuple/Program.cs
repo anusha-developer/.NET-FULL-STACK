268a8af01c5fafff11845306bfc9ae4bcc0f7b27
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-29 22:10:20 +0530

    Add files via upload

---
+++ tuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                var person = Tuple.Create(1, "Steve", "Jobs");
+                DisplayTuple(person);
+            }
+
+            static void DisplayTuple(Tuple<int, string, string> person)
+            {
+                Console.WriteLine($"Id = { person.Item1}");
+                Console.WriteLine($"First Name = { person.Item2}");
+                Console.WriteLine($"Last Name = { person.Item3}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
