24384fdadd8d39896d0943f585cd8cc7dc58bd4a
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-29 20:56:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 
-namespace Duplicates
+namespace method_override
 {
     class Program
     {
+        public virtual void display()
+        {
+            System.Console.WriteLine(" program display");
+
+        }
+    }
+    class program2:Program
+    {
+        public override void display()
+        {
+            System.Console.WriteLine("program2 display");
+        }
+    }
+
+       class Demo { 
         static void Main(string[] args)
         {
-            int Array, temp,i,n;
-            int j = 0;
-            for (i = 0; i <= n - 1; i++) ;
-            {
-                if (Array[i] != Array[i + 1]) ;
-                {
-                    temp[j] = Array[n - 1];
-                    j++;
-                }
-            }
-            temp[j] = Array[n - 1];
-        }
+            Program pgr;
+            pgr = new Program();
+            pgr.display();
 
-           /* int[] array = { 10, 5, 10, 2, 2, 3, 5, 5, 6, 8, 11, 12, 12 };
-            int count = 1;
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i; j < array.Length - 1; j++)
-                {
+            pgr = new program2();
+            pgr.display();
 
-                    if (array[j] == array[j + 1])
-                        count = count + 1;
-                }
-                Console.WriteLine("\n" + array[i] + "values" + count);
-                Console.ReadLine();*/
-            }
-        }
-    
-
+            
+           
+    }
+}
+}
